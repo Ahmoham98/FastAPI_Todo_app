bfd81e1335e30cf391406d25f8407169d2661821
Ahmoham98 <electricallover45@gamil.com>
2026-05-21 17:10:01 +0330

    Add pydantic field_validator, model_validator & cusotm validator (EmailStr here) for input validation

--- core/schemas.py
+++ core/schemas.py
@@ -1,7 +1,16 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, EmailStr, model_validator, field_validator
 
 class BasePersonSchema(BaseModel):
-    name: str
+    name: str # = Field(..., max_length=50)
+
+    @field_validator("name")
+    def validate_name(cls, value):
+        if len(value) > 32:
+            raise ValueError("Name must not exceed 32 characters")
+        if not value.isalpha():
+            raise ValueError("Name must contain only alphabetic characters")
+        return value
+    
 
 class PersonCreateSchema(BasePersonSchema):
     pass
@@ -11,3 +20,18 @@ class PersonResponseSchema(BasePersonSchema):
 
 class PersonUpdateSchema(BasePersonSchema):
     pass
+
+# ROOT VALIDTOR EXAMPLE
+class Boost(BaseModel):
+    start: int 
+    end: int
+
+    @model_validator(mode='after') # mode='after' for post-initialization validation
+    def check_range(self): # 'self' is passed in 'after' mode
+        if self.start >= self.end:
+            raise ValueError("start must be less than end!")
+        return self
+
+# CUSTOM VALIDATOR EXAMPLE
+class User(BaseModel):
+    email: EmailStr
